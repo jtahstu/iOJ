af44a3a2d24f680739e66ad6de80fc59b18afbec
jtahstu <jtahstu@jtahstu-PC>
2016-05-08 09:10:46 +0800

    1.0.3 关于页面

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,28 +1,37 @@
 @extends('layouts')
-@section('top','<a href="#" class="top">↑</a>')
+@section('top','
+<a href="#" class="top">
+	↑
+</a>')
 @section('js','$(function($) {
-	$(".footer").css("margin-top",function(){
-		return $(window).height()-102;
-	});
+$(".footer").css("margin-top",function(){
+return $(window).height()-102;
+});
 });')
 @section('about')
-	<div class="pad60 about">
-		
-		<div class="">
-				<h3>
-					关于ACM
-				</h3>
-				<p class="pad2em">
-					ACM即ACM国际大学生程序设计竞赛（ACM International Collegiate Programming Contest, 简称ACM-ICPC/ICPC）是由国际计算机界具有悠久历史的权威性组织(美国)计算机协会（Association for Computing Machinery, 简称ACM）主办的一项旨在展示大学生创新能力、团队精神和在压力下编写程序、分析和解决问题能力的年度竞赛。
-				</p>
-			</div>
-			<div class="">
-				<h3>
-					关于IOJ
-				</h3>
-				<p class="pad2em">
-					IOJ - 基于PHP+MySQL的OJ题解网站 , 目前涵盖AKOJ，NYOJ，HDUOJ .
-				</p>
-		</div>
+<div class="pad60 about">
+	<div class="">
+		<h3>
+			关于ACM
+		</h3>
+		<p class="pad2em">
+			ACM即ACM国际大学生程序设计竞赛（ACM International Collegiate Programming Contest, 简称ACM-ICPC/ICPC）是由国际计算机界具有悠久历史的权威性组织(美国)计算机协会（Association for Computing Machinery, 简称ACM）主办的一项旨在展示大学生创新能力、团队精神和在压力下编写程序、分析和解决问题能力的年度竞赛。
+		</p>
 	</div>
+	<div class="">
+		<h3>
+			关于IOJ
+		</h3>
+		<p class="pad2em">
+			IOJ - 基于PHP+MySQL的OJ题解网站 , 目前涵盖AKOJ，NYOJ，HDUOJ .
+		</p>
+	</div>
+	<div id="">
+		<img src="{{URL::asset('img/akrank.png')}}" width="100%" style="max-width: 1015px;"/>
+		<br />
+		<img src="{{URL::asset('img/nyrank.png')}}" width="100%" style="max-width: 1015px;"/>
+		<br />
+		<img src="{{URL::asset('img/hdurank.png')}}" width="100%" style="max-width: 1015px;"/>
+	</div>
+</div>
 @endsection
